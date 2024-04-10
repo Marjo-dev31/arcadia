2c2f1500a250d368063de08a7cdc11cf689d29d7
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-10 15:57:10 +0200

    review status

--- frontend/arcadia/src/app/pages/backoffice/component-handled/employed/review-handled.component.ts
+++ frontend/arcadia/src/app/pages/backoffice/component-handled/employed/review-handled.component.ts
@@ -23,6 +23,10 @@ import { ReviewsService } from '../../../home/services/reviews.service';
         <th mat-header-cell *matHeaderCellDef>Date</th>
         <td mat-cell *matCellDef="let review">{{review.date}}</td>
       </ng-container>
+      <ng-container matColumnDef="status">
+        <th mat-header-cell *matHeaderCellDef>Etat actuel</th>
+        <td mat-cell *matCellDef="let review">Publié/Refusé</td>
+      </ng-container>
 
       <ng-container matColumnDef="actions">
         <th mat-header-cell *matHeaderCellDef>Action</th>
@@ -42,7 +46,7 @@ export class ReviewHandledComponent implements OnInit {
   constructor() {}
 
   displayColums: string[] = [
-    'pseudo', 'content', 'date', 'actions'
+    'pseudo', 'content', 'date', 'actions', 'status'
   ];
 
   datasource!: Review[];
