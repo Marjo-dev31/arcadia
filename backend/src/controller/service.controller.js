3a34a02b5e23200beaac6bd86236e5abea068f3a
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-12 11:10:49 +0200

    get service

--- backend/src/controller/service.controller.js
+++ backend/src/controller/service.controller.js
@@ -1,20 +1,104 @@
-import database from '../config/mysql.config.js';
-import Response from '../domain/response.js';
-import logger from '../util/logger.js';
-import QUERY from '../query/service.query.js';
-
+import database from "../config/mysql.config.js";
+import Response from "../domain/response.js";
+import logger from "../util/logger.js";
+import QUERY from "../query/service.query.js";
 
 const httpStatus = {
-    OK: { code: 200, status: 'OK' },
-    CREATE: { code: 201, status: 'CREATED' },
-    NO_CONTENT: { code: 204, status: 'NO_CONTENT' },
-    BAD_REQUEST: { code: 400, status: 'BAD_REQUEST' },
-    NOT_FOUND: { code: 404, status: 'NOT_FOUND' },
-    INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' },
-}
+  OK: { code: 200, status: "OK" },
+  CREATED: { code: 201, status: "CREATED" },
+  NO_CONTENT: { code: 204, status: "NO_CONTENT" },
+  BAD_REQUEST: { code: 400, status: "BAD_REQUEST" },
+  NOT_FOUND: { code: 404, status: "NOT_FOUND" },
+  INTERNAL_SERVER_ERROR: { code: 500, status: "INTERNAL_SERVER_ERROR" },
+};
 
-//  export default getService = (req, res) => {
+export const getServices = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching services`);
+  database.query(QUERY.SELECT_SERVICES, (error, results) => {
+    if (!results) {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `No services found`
+          )
+        );
+    } else {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `Services retrieved`,
+            { services: results }
+          )
+        );
+    }
+  });
+};
 
-// }
+
+export const getService = (req, res) => {
+    logger.info(`${req.method} ${req.originalUrl}, fetching service`);
+    database.query(QUERY.SELECT_SERVICE, [req.params.id], (error, results) => {
+      if (!results[0]) {
+        res
+          .status(httpStatus.NOT_FOUND.code)
+          .send(
+            new Response(
+              httpStatus.NOT_FOUND.code,
+              httpStatus.NOT_FOUND.status,
+              `Service by id ${req.params.id} was not found !`
+            )
+          );
+      } else {
+        res
+          .status(httpStatus.OK.code)
+          .send(
+            new Response(
+              httpStatus.OK.code,
+              httpStatus.OK.status,
+              `Service retrieved`,
+              results[0]
+            )
+          );
+      }
+    });
+  };
+
+
+
+export const addService = (req, res) => {
+    logger.info(`${req.method} ${req.originalUrl}, creating service`);
+    database.query(QUERY.CREATE_SERVICE, Object.values(req.body),(error, results) => {
+      if (!results) {
+        logger.error(error.message);
+        res
+          .status(httpStatus.INTERNAL_SERVER_ERROR.code)
+          .send(
+            new Response(
+              httpStatus.INTERNAL_SERVER_ERROR.code,
+              httpStatus.INTERNAL_SERVER_ERROR.status,
+              `Error occured`
+            )
+          );
+      } else {
+        const service = { ...req.body };
+        res
+          .status(httpStatus.CREATED.code)
+          .send(
+            new Response(
+              httpStatus.CREATED.code,
+              httpStatus.CREATED.status,
+              `Services created`,
+              { service }
+            )
+          );
+      }
+    });
+  };
 
 export default httpStatus;
--- backend/src/query/service.query.js
+++ backend/src/query/service.query.js
@@ -1,9 +1,9 @@
 const QUERY = {
-    SELECT_PATIENTS: 'SELECT * FROM services',
-    SELECT_PATIENT: 'SELECT * FROM services WHERE service_uuid = ?',
-    CREATE_PATIENT: 'INSERT INTO services(service_uuid, title, description) VALUES (UUID(), ?, ?)',
-    UPDATE_PATIENT: 'UPDATE services SET title = ?, description = ? WHERE service_uuid = ?',
-    DELETE_PATIENT: 'DELETE FROM services WHERE service_uuid = ?'
+    SELECT_SERVICES: 'SELECT * FROM services',
+    SELECT_SERVICE: 'SELECT * FROM services WHERE service_uuid = ?',
+    CREATE_SERVICE: 'INSERT INTO services(service_uuid, title, description) VALUES (UUID(), ?, ?)',
+    UPDATE_SERVICE: 'UPDATE services SET title = ?, description = ? WHERE service_uuid = ?',
+    DELETE_SERVICE: 'DELETE FROM services WHERE service_uuid = ?'
 }
 
 export default QUERY;
