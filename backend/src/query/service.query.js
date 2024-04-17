cdb77b895487a486b11382e9fb6b0c39bfed40c2
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-17 15:56:10 +0200

    connection db

--- backend/src/query/service.query.js
+++ backend/src/query/service.query.js
@@ -1,8 +1,8 @@
 const QUERY = {
     SELECT_SERVICES: 'SELECT * FROM services',
     SELECT_SERVICE: 'SELECT * FROM services WHERE id = ?',
-    CREATE_SERVICE: 'INSERT INTO services(id, title, description) VALUES ( ?, ?, ?)',
-    UPDATE_SERVICE: 'UPDATE services SET title = ?, description = ? WHERE id = ?',
+    CREATE_SERVICE: 'INSERT INTO services(id, title, description) VALUES ( DEFAULT, ?, ?)',
+    UPDATE_SERVICE: 'UPDATE services SET title = ?, description = ? WHERE id = ? ',
     DELETE_SERVICE: 'DELETE FROM services WHERE id = ?'
 }
 
