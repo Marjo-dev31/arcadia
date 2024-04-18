534e14ac3f4fbe88ec45c38363327bee678543b2
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-18 14:38:25 +0200

    toggle form

--- frontend/arcadia/src/app/shared/models/service.interface.ts
+++ frontend/arcadia/src/app/shared/models/service.interface.ts
@@ -2,5 +2,12 @@ export interface Service {
     id: string,
     title: string,
     description: string,
+    
     // image: string
+}
+
+
+export interface ServiceCreate {
+    title: string,
+    description: string
 }
