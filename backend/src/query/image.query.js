30c031461de5ce4457f787efb2aab0f8cd4722b2
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-22 18:47:49 +0200

    get image test

--- backend/src/query/image.query.js
+++ backend/src/query/image.query.js
@@ -1,7 +1,7 @@
 const QUERYIMAGES = {
     SELECT_IMAGES: 'SELECT * FROM images',
     SELECT_IMAGE: 'SELECT * FROM images WHERE id = ?',
-    CREATE_IMAGE: 'INSERT INTO images(id, image_url, id_animal, id_habitat, id_service ) VALUES ( DEFAULT, ?, NULL, NULL, NULL)',
+    CREATE_IMAGE_SERVICE: 'INSERT INTO images(id, image_url,id_service ) VALUES ( DEFAULT, ?, ?)',
     UPDATE_IMAGE: 'UPDATE images SET image_url = ?, id_animal = ?, id_habitat= ?, id_service= ? WHERE id = ? ',
     DELETE_IMAGE: 'DELETE FROM images WHERE id = ?'
 }
