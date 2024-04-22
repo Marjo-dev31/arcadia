91fd6ce4fc8b2ca1c518b8ec2936f7cd373931c2
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-22 18:47:49 +0200

    get image test

--- backend/src/controller/image.controller.js
+++ backend/src/controller/image.controller.js
@@ -5,11 +5,66 @@ import Response from "../domain/response.js";
 import httpStatus from "./service.controller.js";
 
 
+export const getImages = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching images get`);
+  database.query(QUERYIMAGES.SELECT_IMAGES, (error, results) => {
+    if (!results) {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `No images found`
+          )
+        );
+    } else {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `Images retrieved`,
+            { images: results }
+          )
+        );
+    }
+  });
+};
 
+
+export const getImage = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching image get`);
+  database.query(QUERYIMAGES.SELECT_IMAGE, [req.params.id], (error, results) => {
+    if (!results) {
+      res
+        .status(httpStatus.NOT_FOUND.code)
+        .send(
+          new Response(
+            httpStatus.NOT_FOUND.code,
+            httpStatus.NOT_FOUND.status,
+            `Image by id ${req.params.id} was not found !`
+          )
+        );
+    } else {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `Image retrieved`,
+            results[0]
+          )
+        );
+    }
+  });
+};
 
 export const addImage = (req, res) => {
     logger.info(`${req.method} ${req.originalUrl}, creating image post`);
-    database.query(QUERYIMAGES.CREATE_IMAGE, Object.values(req.body),(error, results) => {
+    database.query(QUERYIMAGES.CREATE_IMAGE_SERVICE, Object.values(req.body),(error, results) => {
       if (!results) {
         logger.error(error.message);
         res
