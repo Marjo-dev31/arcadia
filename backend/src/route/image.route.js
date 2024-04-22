90c55e440a59ae98bf465a646831c92290e3235c
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-22 18:47:49 +0200

    get image test

--- backend/src/route/image.route.js
+++ backend/src/route/image.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { addImage } from "../controller/image.controller.js";
+import { addImage, getImage, getImages } from "../controller/image.controller.js";
 
 
 const imageRoutes = express.Router();
 
 imageRoutes.route('/')
+.get(getImages)
 .post(addImage);
+
+imageRoutes.route('/:id')
+.get(getImage)
 
 
 export default imageRoutes
