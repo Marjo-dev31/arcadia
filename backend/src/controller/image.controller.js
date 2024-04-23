d97fefaa742359338410f3cdada940a5c3e91f89
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-23 11:43:56 +0200

    image service

--- backend/src/controller/image.controller.js
+++ backend/src/controller/image.controller.js
@@ -1,12 +1,126 @@
-import database from "../config/mysql.config.js"
+import database from "../config/mysql.config.js";
 import QUERYIMAGES from "../query/image.query.js";
 import logger from "../util/logger.js";
 import Response from "../domain/response.js";
 import httpStatus from "./service.controller.js";
 
+export const getServicesImages = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching images list `);
+  database.query(QUERYIMAGES.SELECT_SERVICES_IMAGES, (error, results) => {
+    if (!results) {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `No images found`
+          )
+        );
+    } else {
+      res
+        .status(httpStatus.OK.code)
+        .send(
+          new Response(
+            httpStatus.OK.code,
+            httpStatus.OK.status,
+            `Images retrieved`,
+            { images: results }
+          )
+        );
+    }
+  });
+};
+
+export const addServiceImage = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, creating image `);
+  database.query(
+    QUERYIMAGES.CREATE_IMAGE_SERVICE,
+    Object.values(req.body),
+    (error, results) => {
+      if (!results) {
+        logger.error(error.message);
+        res
+          .status(httpStatus.INTERNAL_SERVER_ERROR.code)
+          .send(
+            new Response(
+              httpStatus.INTERNAL_SERVER_ERROR.code,
+              httpStatus.INTERNAL_SERVER_ERROR.status,
+              `Error occured`
+            )
+          );
+      } else {
+        const image = { ...req.body };
+        res
+          .status(httpStatus.CREATED.code)
+          .send(
+            new Response(
+              httpStatus.CREATED.code,
+              httpStatus.CREATED.status,
+              `Image created`,
+              { image }
+            )
+          );
+      }
+    }
+  );
+};
+
+export const updateServiceImage = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching image `);
+  database.query(
+    QUERYIMAGES.SELECT_IMAGE,
+    [req.params.id],
+    (error, results) => {
+      console.log(results);
+      if (!results[0]) {
+        res
+          .status(httpStatus.NOT_FOUND.code)
+          .send(
+            new Response(
+              httpStatus.NOT_FOUND.code,
+              httpStatus.NOT_FOUND.status,
+              `Image by id ${req.params.id} was not found !`
+            )
+          );
+      } else {
+        logger.info(`${req.method} ${req.originalUrl}, updating image`);
+        database.query(
+          QUERYIMAGES.UPDATE_SERVICE_IMAGE,
+          [...Object.values(req.body), req.params.id],
+          (error, results) => {
+            if (!error) {
+              res
+                .status(httpStatus.OK.code)
+                .send(
+                  new Response(
+                    httpStatus.OK.code,
+                    httpStatus.OK.status,
+                    `Image updated`,
+                    { ...req.body }
+                  )
+                );
+            } else {
+              logger.error(error.message);
+              res
+                .status(httpStatus.INTERNAL_SERVER_ERROR.code)
+                .send(
+                  new Response(
+                    httpStatus.INTERNAL_SERVER_ERROR.code,
+                    httpStatus.INTERNAL_SERVER_ERROR.status,
+                    `Error occured`
+                  )
+                );
+            }
+          }
+        );
+      }
+    }
+  );
+};
 
 export const getImages = (req, res) => {
-  logger.info(`${req.method} ${req.originalUrl}, fetching images get`);
+  logger.info(`${req.method} ${req.originalUrl}, fetching images `);
   database.query(QUERYIMAGES.SELECT_IMAGES, (error, results) => {
     if (!results) {
       res
@@ -33,64 +147,34 @@ export const getImages = (req, res) => {
   });
 };
 
-
 export const getImage = (req, res) => {
-  logger.info(`${req.method} ${req.originalUrl}, fetching image get`);
-  database.query(QUERYIMAGES.SELECT_IMAGE, [req.params.id], (error, results) => {
-    if (!results) {
-      res
-        .status(httpStatus.NOT_FOUND.code)
-        .send(
-          new Response(
-            httpStatus.NOT_FOUND.code,
-            httpStatus.NOT_FOUND.status,
-            `Image by id ${req.params.id} was not found !`
-          )
-        );
-    } else {
-      res
-        .status(httpStatus.OK.code)
-        .send(
-          new Response(
-            httpStatus.OK.code,
-            httpStatus.OK.status,
-            `Image retrieved`,
-            results[0]
-          )
-        );
-    }
-  });
-};
-
-export const addImage = (req, res) => {
-    logger.info(`${req.method} ${req.originalUrl}, creating image post`);
-    database.query(QUERYIMAGES.CREATE_IMAGE_SERVICE, Object.values(req.body),(error, results) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching image `);
+  database.query(
+    QUERYIMAGES.SELECT_IMAGE,
+    [req.params.id],
+    (error, results) => {
       if (!results) {
-        logger.error(error.message);
         res
-          .status(httpStatus.INTERNAL_SERVER_ERROR.code)
+          .status(httpStatus.NOT_FOUND.code)
           .send(
             new Response(
-              httpStatus.INTERNAL_SERVER_ERROR.code,
-              httpStatus.INTERNAL_SERVER_ERROR.status,
-              `Error occured`
+              httpStatus.NOT_FOUND.code,
+              httpStatus.NOT_FOUND.status,
+              `Image by id ${req.params.id} was not found !`
             )
           );
       } else {
-        const image = {...req.body};
         res
-          .status(httpStatus.CREATED.code)
+          .status(httpStatus.OK.code)
           .send(
             new Response(
-              httpStatus.CREATED.code,
-              httpStatus.CREATED.status,
-              `Image created`,
-              { image }
+              httpStatus.OK.code,
+              httpStatus.OK.status,
+              `Image retrieved`,
+              results[0]
             )
           );
       }
-    });
-  };
-
-
-
+    }
+  );
+};
--- backend/src/query/image.query.js
+++ backend/src/query/image.query.js
@@ -1,9 +1,10 @@
 const QUERYIMAGES = {
     SELECT_IMAGES: 'SELECT * FROM images',
     SELECT_IMAGE: 'SELECT * FROM images WHERE id = ?',
-    CREATE_IMAGE_SERVICE: 'INSERT INTO images(id, image_url,id_service ) VALUES ( DEFAULT, ?, ?)',
-    UPDATE_IMAGE: 'UPDATE images SET image_url = ?, id_animal = ?, id_habitat= ?, id_service= ? WHERE id = ? ',
-    DELETE_IMAGE: 'DELETE FROM images WHERE id = ?'
+    DELETE_IMAGE: 'DELETE FROM images WHERE id = ?',
+    SELECT_SERVICES_IMAGES: 'SELECT * FROM services INNER JOIN images ON services.id = images.id_service',
+    CREATE_IMAGE_SERVICE: 'INSERT INTO images(id, image_url, id_service ) VALUES ( DEFAULT, ?, ?)',
+    UPDATE_SERVICE_IMAGE: 'UPDATE images SET image_url = ?, id_service= ? WHERE id = ? '
 }
 
 export default QUERYIMAGES;
