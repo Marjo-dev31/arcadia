56474361df07cce638b31c67bbe36ff7b15040d8
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-26 16:44:44 +0200

    LEFT JOIN

--- backend/src/query/habitat.query.js
+++ backend/src/query/habitat.query.js
@@ -1,5 +1,5 @@
 const QUERYHABITATS = {
-    SELECT_HABITATS:'SELECT title, description, habitats.id, images.image_url FROM habitats INNER JOIN images ON habitats.id = images.id_habitat',
+    SELECT_HABITATS:'SELECT title, description, habitats.id, images.image_url FROM habitats LEFT JOIN images ON habitats.id = images.id_habitat',
     SELECT_HABITAT: 'SELECT * FROM habitats WHERE id = ?',
     CREATE_HABITAT: 'INSERT INTO habitats(id, title, description) VALUES (DEFAULT, ?, ?)',
     CREATE_COMMENT_HABITAT: 'UPDATE habitats SET comment = ? WHERE id= ?',
