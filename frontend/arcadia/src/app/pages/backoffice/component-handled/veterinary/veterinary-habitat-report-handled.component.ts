3a4e456e37e888008455f2617fcfb0c242be487d
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-29 15:14:46 +0200

    handle comment ok

--- frontend/arcadia/src/app/pages/backoffice/component-handled/veterinary/veterinary-habitat-report-handled.component.ts
+++ frontend/arcadia/src/app/pages/backoffice/component-handled/veterinary/veterinary-habitat-report-handled.component.ts
@@ -3,12 +3,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { HabitatsService } from '../../../habitats/services/habitat.service';
 import { Habitat } from '../../../../shared/models';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
+import { NgStyle } from '@angular/common';
 
 
 @Component({
   selector: 'app-veterinary-habitat-report-handled',
   standalone: true,
-  imports: [MatTableModule, MatIconModule],
+  imports: [MatTableModule, MatIconModule, ReactiveFormsModule, NgStyle],
   template: `
   <section class="habitats-section">
     <h3>Section habitats</h3>
@@ -18,53 +21,82 @@ import { Habitat } from '../../../../shared/models';
         <td mat-cell *matCellDef="let habitat"> {{habitat.title}}</td>
       </ng-container>
       <ng-container matColumnDef="comment">
-        <th mat-header-cell *matHeaderCellDef>Commentaire</th>
-        <td mat-cell *matCellDef></td>
+        <th mat-header-cell *matHeaderCellDef>Dernier commentaire</th>
+        <td mat-cell *matCellDef="let habitat"> {{habitat.comment}} </td>
       </ng-container>
       <ng-container matColumnDef="actions">
         <th mat-header-cell *matHeaderCellDef>Action</th>
-        <td mat-cell *matCellDef>
-          <mat-icon>create</mat-icon>
-          <mat-icon>delete</mat-icon>
+        <td mat-cell *matCellDef="let habitat">
+          <!-- <mat-icon>create</mat-icon> -->
+          <mat-icon (click)="deleteComment(habitat.id)">delete</mat-icon>
         </td>
       </ng-container>
       <tr mat-header-row *matHeaderRowDef="displayColums"></tr>
       <tr mat-row *matRowDef="let row; columns: displayColums"></tr>
     </table>
-    <mat-icon class="add-icon">add_circle_outline</mat-icon>
+    <mat-icon class="add-icon" (click)="toggleAddForm()">add_circle_outline</mat-icon>
   </section>
-  <section>
-    <form>
-      <select name="habitats" id="habitat-select">
+  <section [ngStyle]="{ display: addFormIsDisplay ? 'block' : 'none' }">
+    <form [formGroup]="commentForm" (ngSubmit)="onSubmit(habitatSelected.value)">
+      <select name="habitats" id="habitat-selected" #habitatSelected>
+        <option>-- Choissisez un habitat --</option>
         @for(habitat of datasource; track habitat){
-        <option value="{{habitat.id}}">{{habitat.title}}</option>}
+        <option [value]="habitat.id">{{habitat.title}}</option>
+      }
       </select>
-    <textarea name="" id="" cols="30" rows="5" placeholder="Ajouter votre commentaire ici"></textarea>
+    <textarea formControlName="comment" cols="30" rows="5" placeholder="Ajouter votre commentaire ici"></textarea>
+    <button >Ajouter mon commentaire</button>
     </form>
   </section>
   `,
   styleUrl: `../component-handled.component.css`,
 })
 export class VeterinaryHabitatReportHandledComponent implements OnInit {
-  constructor() {}
+
+  public commentForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.commentForm = this.fb.group({
+      comment: new FormControl('')
+    })
+  }
 
   displayColums: string[] = [
   'habitat',
   'comment',
   'actions'
-]
+];
+
+  datasource!: Habitat[];
 
   report!: string[]
 
 
-  datasource!: Habitat[];
   private readonly habitatService = inject(HabitatsService);
 
+  addFormIsDisplay: boolean = false
+
   ngOnInit() {
-    this.habitatService.getHabitats().then((response) => {
-      this.datasource = response;
-    });
+    this.getHabitats()
   }
 
+    getHabitats() {
+      this.habitatService.getHabitats().then((response) => {
+      this.datasource = response;
+    })}
+  
+    toggleAddForm(){
+      this.addFormIsDisplay = !this.addFormIsDisplay
+    }
 
+    onSubmit(id: string) {
+      this.habitatService.addComment(this.commentForm.value, id).pipe(tap(()=> {this.getHabitats()})).subscribe();
+      this.commentForm.reset();
+      this.addFormIsDisplay = !this.addFormIsDisplay;
+    }
+
+    deleteComment(id: string) {
+      this.habitatService.deleteComment(id).pipe(tap(()=> {this.getHabitats()})).subscribe();
+    }
+    
 }
