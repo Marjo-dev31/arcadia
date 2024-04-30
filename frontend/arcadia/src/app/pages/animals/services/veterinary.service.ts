f62c2222d84a6b14bf8524ab16f4df4185d15f73
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-04-30 17:08:40 +0200

    veterinary service

---
+++ frontend/arcadia/src/app/pages/animals/services/veterinary.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class VeterinaryService {
+    url = 'http://localhost:8000/veterinaries';
+
+    constructor(private http: HttpClient){}
+
+    getVeterinaryReports(): Observable<any>{
+        return this.http.get(this.url)
+    }
+
+}
