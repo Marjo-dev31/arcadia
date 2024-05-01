3cbca1aba2986cc6d4a8f1e403dda4595ea88e9c
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-01 09:51:22 +0200

    update done

--- backend/src/query/veterinary.query.js
+++ backend/src/query/veterinary.query.js
@@ -1,7 +1,8 @@
 const QUERYVETERINARIES = {
     SELECT_REPORTS:'SELECT * FROM veterinary_reports WHERE id_animal = ?',
-    SELECT_REPORT:'SELCT * FROM veterinary_reports WHERE id = ?',
+    SELECT_REPORT:'SELECT * FROM veterinary_reports WHERE id = ?',
     CREATE_REPORT: 'INSERT INTO veterinary_reports values (DEFAULT, ?, ?, CURDATE(), ?, ?, ?, ?)',
+    UPDATE_REPORT: 'UPDATE veterinary_reports SET food = ?, grammage = ?, health = ?, details_condition = ?, id_user = ?, id_animal = ? WHERE id = ?',
     DELETE_REPORT: 'DELETE FROM veterinary_reports WHERE id = ?'
 }
 
