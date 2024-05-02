fe960ab93482f82750c9096bb720c57d78bacb71
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-02 10:17:04 +0200

    update report employ front

--- backend/src/query/employee.query.js
+++ backend/src/query/employee.query.js
@@ -2,7 +2,7 @@ const QUERYEMPLOYEES = {
     SELECT_REPORTS:'SELECT * FROM employee_reports WHERE id_animal = ?',
     SELECT_REPORT:'SELECT * FROM employee_reports WHERE id = ?',
     CREATE_REPORT:'INSERT INTO employee_reports VALUES (DEFAULT, ?, ?, NOW(), ?, ?)',
-    UPDATE_REPORT:'UPDATE employee_reports SET food = ?, grammage = ?, id_animal = ?,  id_user = ? WHERE id = ?',
+    UPDATE_REPORT:'UPDATE employee_reports SET food = ?, grammage = ?, id_user = ?, id_animal = ? WHERE id = ?',
     DELETE_REPORT:'DELETE FROM employee_reports WHERE id = ?'
 }
 
