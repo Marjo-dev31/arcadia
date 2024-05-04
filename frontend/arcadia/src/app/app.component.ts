3ef350cdcdc7580011b9b038b716cafcfc4f61b5
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-04 18:07:04 +0200

    openingcomponent

--- frontend/arcadia/src/app/app.component.ts
+++ frontend/arcadia/src/app/app.component.ts
@@ -25,11 +25,12 @@ import { EmployeeService } from './pages/animals/services/employee.service';
 import { RoleService } from './pages/login/service/role.service';
 import { LoginService } from './pages/login/service/login.service';
 import { ClickService } from './pages/animals/services/click.service';
+import { OpeningService } from './pages/services/service/opening.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers: [ServiceService, HabitatsService, AnimalService, ReviewsService, ImageService, BreedService, VeterinaryService, UserService, EmployeeService, RoleService, LoginService, ClickService],
+  providers: [ServiceService, HabitatsService, AnimalService, ReviewsService, ImageService, BreedService, VeterinaryService, UserService, EmployeeService, RoleService, LoginService, ClickService, OpeningService],
   imports: [
     RouterOutlet,
     HeaderComponent,
