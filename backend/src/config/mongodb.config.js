d9146b3b7cfc6e77c776e16ecd509d8d5867bc13
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-04 17:05:14 +0200

    update opening

--- backend/src/config/mongodb.config.js
+++ backend/src/config/mongodb.config.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'localhost:27017/test'
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/test`
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI, {dbName: "arcadia"});
+        await mongoose.connect(MONGODB_URI, {dbName: "arcadia"})
         console.log('MongoDb is connected!')
     } catch(error) {
         console.log(error);
---
+++ backend/src/models/opening.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const openingSchema = new mongoose.Schema({
+    openingTime: {
+        type: String
+    },
+    closingTime: {
+        type: String
+    },
+    openingDay: {
+        type: String
+    },
+    closingTime: {
+        type: String
+    },
+});
+
+const openingModel = mongoose.model('opening', openingSchema, 'opening');
+
+export default openingModel
