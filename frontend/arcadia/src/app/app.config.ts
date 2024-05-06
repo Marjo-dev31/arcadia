24a3a5e7c10c8ca3b8fa2e5099cdb36c73d84761
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-06 18:56:03 +0200

    interceptor

--- frontend/arcadia/src/app/app.config.ts
+++ frontend/arcadia/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { httpInterceptorProviders } from './shared/interceptors';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
+
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), httpInterceptorProviders]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor]))]
 };
