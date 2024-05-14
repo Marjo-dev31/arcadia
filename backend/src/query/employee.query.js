827a63393e675418e7893ff552cd7792447b7c8a
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-14 12:07:58 +0200

    test and date report

--- backend/src/query/employee.query.js
+++ backend/src/query/employee.query.js
@@ -1,5 +1,5 @@
 const QUERYEMPLOYEES = {
-    SELECT_REPORTS:'SELECT * FROM employee_reports WHERE id_animal = ?',
+    SELECT_REPORTS:'SELECT id, food, grammage, DATE_FORMAT(date,"%d/%m/%Y à %H:%i:%s") AS date, id_user, id_animal FROM employee_reports WHERE id_animal = ?',
     SELECT_REPORT:'SELECT * FROM employee_reports WHERE id = ?',
     CREATE_REPORT:'INSERT INTO employee_reports VALUES (DEFAULT, ?, ?, NOW(), ?, ?)',
     UPDATE_REPORT:'UPDATE employee_reports SET food = ?, grammage = ?, id_user = ?, id_animal = ? WHERE id = ?',
