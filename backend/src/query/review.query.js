752f75d1f949086fe47ca692959c79cf29e2035e
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-16 10:49:34 +0200

    rebuild docker

--- backend/src/query/review.query.js
+++ backend/src/query/review.query.js
@@ -1,5 +1,5 @@
 const QUERYREVIEWS = {
-    SELECT_REVIEWS: 'SELECT id, pseudo, content, DATE_FORMAT(date,"%d/%m/%Y") as date, status FROM reviews',
+    SELECT_REVIEWS: 'SELECT id, pseudo, content, DATE_FORMAT(date,"%d/%m/%Y") AS date, status FROM reviews',
     SELECT_REVIEW:'SELECT * FROM reviews WHERE id = ?',
     CREATE_REVIEW: 'INSERT INTO reviews(id, pseudo, content, date, status) VALUES (DEFAULT, ?, ?, CURDATE(), false)',
     UPDATE_REVIEW_STATUS: 'UPDATE reviews SET status = ? WHERE id = ?'
