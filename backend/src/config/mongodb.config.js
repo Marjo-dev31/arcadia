cea058ea182a11f20a3e57c3666a4e26e7e24cdd
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-16 18:08:06 +0200

    prod

--- backend/src/config/mongodb.config.js
+++ backend/src/config/mongodb.config.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
         console.log(`MongoDb is connected! on ${MONGODB_URI}`)
     } catch(error) {
         console.log(error);
-        process.exit(1)
+        // process.exit(1)
     }
 };
 
