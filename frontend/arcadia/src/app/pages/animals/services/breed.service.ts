c8b310cc2ead86769a5dc665c7f2882f8ceeb91c
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-06-21 15:27:58 +0200

    response<T> create

--- frontend/arcadia/src/app/pages/animals/services/breed.service.ts
+++ frontend/arcadia/src/app/pages/animals/services/breed.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { BreedCreate } from "../../../shared/models/breed.interface";
 import { environment } from '../../../environments/environment';
-import { ResponseBreed } from "../../../shared/models/response.interface";
+
 
 
 @Injectable()
@@ -12,12 +12,12 @@ export class BreedService {
 
     constructor(private http: HttpClient) {}
 
-    getBreeds(): Observable<ResponseBreed> {
-        return this.http.get<ResponseBreed>(this.url);
+    getBreeds(): Observable<any> {
+        return this.http.get(this.url);
     }
 
-    addBreed(breed: BreedCreate): Observable<ResponseBreed> {
-        return this.http.post<ResponseBreed>(this.url, breed)
+    addBreed(breed: BreedCreate): Observable<any> {
+        return this.http.post(this.url, breed)
     }
 
 }
