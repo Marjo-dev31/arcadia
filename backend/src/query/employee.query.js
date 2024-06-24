d44803967eebf14ca225f6d485eb95109e97ad79
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-06-24 14:27:18 +0200

    done

--- backend/src/query/employee.query.js
+++ backend/src/query/employee.query.js
@@ -1,9 +1,10 @@
 const QUERYEMPLOYEES = {
-    SELECT_REPORTS:'SELECT id, food, grammage, DATE_FORMAT(date,"%d/%m/%Y à %H:%i:%s") AS date, id_user, id_animal FROM employee_reports WHERE id_animal = ?',
-    SELECT_REPORT:'SELECT * FROM employee_reports WHERE id = ?',
-    CREATE_REPORT:'INSERT INTO employee_reports VALUES (DEFAULT, ?, ?, NOW(), ?, ?)',
-    UPDATE_REPORT:'UPDATE employee_reports SET food = ?, grammage = ?, id_user = ?, id_animal = ? WHERE id = ?',
-    DELETE_REPORT:'DELETE FROM employee_reports WHERE id = ?'
+    SELECT_REPORTS:'SELECT reports.id, food, grammage, DATE_FORMAT(date,"%d/%m/%Y à %H:%i:%s") AS date, id_user, id_animal FROM reports INNER JOIN users ON id_user=users.id WHERE id_animal = ? AND id_role = (SELECT id FROM roles WHERE name = "Employé") ',
+    SELECT_REPORT:'SELECT * FROM reports WHERE id = ?',
+    CREATE_REPORT:'INSERT INTO reports VALUES (DEFAULT, ?, ?, NOW(), DEFAULT, DEFAULT, ?, ?)',
+    UPDATE_REPORT:'UPDATE reports SET food = ?, grammage = ?, id_user = ?, id_animal = ? WHERE id = ?',
+    DELETE_REPORT:'DELETE FROM reports WHERE id = ?'
 }
 
 export default QUERYEMPLOYEES;
+
