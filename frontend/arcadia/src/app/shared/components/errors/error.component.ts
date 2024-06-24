b4067d7148fd792f9bad6fc7050b02a84b2c95c6
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-06-24 10:07:04 +0200

    title error page

--- frontend/arcadia/src/app/shared/components/errors/error.component.ts
+++ frontend/arcadia/src/app/shared/components/errors/error.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-error',
     template: `
+    <h1 class="title">ARCADIA</h1>
     <h3>Oups, une erreur est survenue, veuillez ré-essayer plus tard ou vérifiez que vous êtes bien connecté.</h3>
     `
 })
