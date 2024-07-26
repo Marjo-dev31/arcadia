23054429b0f1a999e068f64dfdd5a92df30b395b
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-07-26 18:32:44 +0200

    logout in interceptor http

--- frontend/arcadia/src/app/shared/interceptors/error.interceptor.ts
+++ frontend/arcadia/src/app/shared/interceptors/error.interceptor.ts
@@ -1,18 +1,18 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
-import { Injector, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, catchError } from 'rxjs';
 import { LoginService } from '../../pages/login/service/login.service';
 
+
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
-  const logginService = inject(LoginService)
+  
   return next(req).pipe(
     catchError((err: HttpErrorResponse): Observable<any> => {
+      const loginService = inject(LoginService)
       router.navigateByUrl('/erreur');
-      localStorage.removeItem('accessToken');
-      localStorage.removeItem('role');
-      logginService.isLoggin = false;
+      loginService.logout()
       throw err;
     })
   );
