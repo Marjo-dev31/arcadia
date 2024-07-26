42514e3ca0e6b7bc5a85473d4f6e0de6c406bf83
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-07-26 17:48:06 +0200

    isloggin false if error http

--- frontend/arcadia/src/app/shared/interceptors/error.interceptor.ts
+++ frontend/arcadia/src/app/shared/interceptors/error.interceptor.ts
@@ -1,15 +1,18 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
+import { Injector, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, catchError } from 'rxjs';
+import { LoginService } from '../../pages/login/service/login.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
+  const logginService = inject(LoginService)
   return next(req).pipe(
     catchError((err: HttpErrorResponse): Observable<any> => {
       router.navigateByUrl('/erreur');
       localStorage.removeItem('accessToken');
       localStorage.removeItem('role');
+      logginService.isLoggin = false;
       throw err;
     })
   );
