f796ea176a741ff1c52cdcb444d2034d8d893968
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-08-06 09:59:24 +0200

    review

--- frontend/arcadia/src/app/app.config.ts
+++ frontend/arcadia/src/app/app.config.ts
@@ -1,14 +1,17 @@
-import { ApplicationConfig} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './shared/interceptors/auth.interceptor';
-import { errorInterceptor } from './shared/interceptors/error.interceptor';
-
-
-
+import { ApplicationConfig } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { routes } from './app.routes'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { authInterceptor } from './shared/interceptors/auth.interceptor'
+import { errorInterceptor } from './shared/interceptors/error.interceptor'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))]
-};
+    providers: [
+        provideRouter(routes),
+        provideAnimationsAsync(),
+        provideHttpClient(
+            withInterceptors([authInterceptor, errorInterceptor])
+        ),
+    ],
+}
