b5563b5346214a2acd4049327b622f66b47c8370
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-09-12 10:44:58 +0200

    fix(html): add link in footer politique de confidentialité

--- backend/src/config/mongodb.config.js
+++ backend/src/config/mongodb.config.js
@@ -6,7 +6,7 @@ const MONGODB_URI = process.env.MONGODB_URI
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGODB_URI, {dbName: "arcadia"})
-        logger.info(`MongoDb is connected! on ${MONGODB_URI}`)
+        logger.info(`MongoDb is connected!`)
     } catch(error) {
         logger.info(error);
         process.exit(1)
