86d95f16d8f0766d0cfbeb511fa31858aae9564a
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-09-23 11:52:40 +0200

    feat(security): add secret manager service

--- backend/src/config/mysql.config.js
+++ backend/src/config/mysql.config.js
@@ -1,11 +1,12 @@
 import mysql from "mysql";
 import dotenv from "dotenv";
+import secrets from '../util/secretmanager.js'
 
 dotenv.config();
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: secrets().DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
