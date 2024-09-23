aa835c5dff83e93f346e6e2d8bbf6c559142c880
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-09-23 11:52:40 +0200

    feat(security): add secret manager service

---
+++ backend/src/util/secretmanager.js
@@ -0,0 +1,41 @@
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'prod';
+
+const secrets = async () => {
+    if (!IS_PRODUCTION) {
+        return {
+            DB_HOST: process.env.DB_HOST,
+        };
+    } else {
+        const secret_name = "prod-arcadia";
+
+        const client = new SecretsManagerClient({
+            region: "eu-west-3",
+        });
+
+        let response;
+
+        try {
+            response = await client.send(
+                new GetSecretValueCommand({
+                    SecretId: secret_name,
+                    VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+                })
+            );
+        } catch (error) {
+            throw error;
+        }
+
+        const secret = response.SecretString;
+
+        return {
+            DB_HOST: secret.DB_HOST,
+        };
+    }
+};
+
+export default secrets;
